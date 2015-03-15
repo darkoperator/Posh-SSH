df1a7798e2a6d5a3563da8c19bf6bd3978e736bc
Carlos Perez <carlos_perez@darkoperator.com>
2015-03-15 10:18:21 -0400

    Initial commit of functions to work with shell streams

--- Posh-SSH.psm1
+++ Posh-SSH.psm1
@@ -261,8 +261,6 @@ function Invoke-SSHCommand
 }
 
 
-
-
 # .ExternalHelp Posh-SSH.psm1-Help.xml
  function Get-PoshSSHModVersion
  {
@@ -307,3 +305,342 @@ function Invoke-SSHCommand
  }
 
 
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function New-SSHShellStream
+{
+    [CmdletBinding(DefaultParameterSetName="Index")]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+            ParameterSetName = "Session",
+            ValueFromPipeline=$true,
+            Position=0)]
+        [Alias("Session")]
+        [SSH.SSHSession]
+        $SSHSession,
+
+        [Parameter(Mandatory=$true,
+            ParameterSetName = "Index",
+            ValueFromPipeline=$true,
+            Position=0)]
+        [Alias('Index')]
+        [Int32]
+        $SessionId,
+
+        # Name of the terminal.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true)]
+        [string]
+        $TerminalName = "PoshSSHTerm",
+
+        # The columns
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true)]
+        [int]
+        $Colums=80,
+
+        # The rows.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true)]
+        [int]
+        $Rows=24,
+
+        # The width.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true)]
+        [int]
+        $With= 800,
+
+        # The height.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true)]
+        [int]
+        $Height=600,
+
+        # Size of the buffer.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true)]
+        [int]
+        $BufferSize=1000
+    )
+
+    Begin
+    {
+        $ToProcess = $null
+        switch($PSCmdlet.ParameterSetName)
+        {
+            'Session'
+            {
+                $ToProcess = $SSHSession
+            }
+
+            'Index'
+            {
+                $sess = Get-SSHSession -Index $SessionId
+                if ($sess)
+                {
+                    $ToProcess = $sess
+                }
+                else
+                {
+                    Write-Error -Message "Session specified with Index $($SessionId) was not found"
+                    return
+                }
+            }
+        }
+    }
+    Process
+    {
+        $ToProcess.Session.CreateShellStream($TerminalName, $Colums, $Rows, $With, $Height, $BufferSize)
+    }
+    End
+    {
+    }
+}
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Invoke-SSHStreamExpectAction
+{
+    [CmdletBinding(DefaultParameterSetName='String')]
+    Param
+    (
+        # SSH Shell Stream. 
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   ValueFromPipeline=$true,
+                   Position=0)]
+        [Renci.SshNet.ShellStream]
+        $ShellStream,
+
+        # Initial command that will generate the output to be evaluated by the expect pattern.
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+        [string]
+        $Command,
+
+        # String on what to trigger the action on.
+        [Parameter(Mandatory=$true,
+                   ParameterSetName='String',
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=2)]
+        [string]
+        $ExpectString,
+
+        # Regular expression on what to trigger the action on.
+        [Parameter(Mandatory=$true,
+                   ParameterSetName='Regex',
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=2)]
+        [regex]
+        $ExpectRegex,
+
+        # Command to execute once an expression is matched.
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=3)]
+        [string]
+        $Action,
+
+        # Number of seconds to wait for a match.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=4)]
+        [int]
+        $TimeOut = 10
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        Write-Verbose -Message "Executing command $($Command)."
+        $ShellStream.WriteLine($Command)
+        Write-Verbose -Message "Waiting for match."
+        switch ($PSCmdlet.ParameterSetName)
+        {
+            'string' {$found = $ShellStream.Expect($ExpectString, (New-TimeSpan -Seconds $TimeOut))}
+            'Regex'  {$found = $ShellStream.Expect($ExpectRegex, (New-TimeSpan -Seconds $TimeOut))}
+        }
+        
+        if ($found -ne $null)
+        {
+            Write-Verbose -Message "Executing action: $($Action)."
+            $ShellStream.WriteLine($Action)
+            Write-Verbose -Message 'Action has been executed.'
+        }
+        else
+        {
+            Write-Warning -Message 'Expect timeout without achiving a match.'
+        }
+    }
+    End
+    {
+    }
+}
+
+<#
+.Synopsis
+   Executes an action stored in a secure string when an output is matched. 
+.DESCRIPTION
+   Executes an action stored in a secure string when an output is matched. By
+   the expect action being in a secure string this function is best used when a
+   password must be provided to a promp protecting it in memory. Examples 
+   uses would be for use in su or sudo commands.
+.EXAMPLE
+
+    Invoke-SSHStreamExpectSecureAction -ShellStream $stream -Command 'su -' -ExpectString 'Passord:' -SecureAction (read-host -AsSecureString) -Verbose
+    ***********
+    VERBOSE: Executing command su -.
+    VERBOSE: Waiting for match.
+    VERBOSE: Executing action.
+    VERBOSE: Action has been executed.
+    PS C:\> $stream.Read()
+
+    Last login: Sat Mar 14 18:18:52 EDT 2015 on pts/0
+    Last failed login: Sun Mar 15 08:52:07 EDT 2015 on pts/0
+    There were 2 failed login attempts since the last successful login.
+    [root@localhost ~]#
+
+.EXAMPLE
+     $sudoPassPropmp = [regex]':\s$'
+    PS C:\> Invoke-SSHStreamExpectSecureAction -ShellStream $stream -Command 'sudo ifconfig' -ExpectRege
+    x $sudoPassPropmp -SecureAction (read-host -AsSecureString) -Verbose
+    ***********
+    VERBOSE: Executing command sudo ifconfig.
+    VERBOSE: Waiting for match.
+    VERBOSE: Matching by RegEx.
+    VERBOSE: Executing action.
+    VERBOSE: Action has been executed.
+    PS C:\> $stream.Read()
+
+    [sudo] password for admin:
+    ens192: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
+            inet 192.168.1.180  netmask 255.255.240.0  broadcast 192.168.15.255
+            inet6 fe80::20c:29ff:feeb:34a0  prefixlen 64  scopeid 0x20<link>
+            ether 00:0c:29:eb:34:a0  txqueuelen 1000  (Ethernet)
+            RX packets 300644  bytes 175076049 (166.9 MiB)
+            RX errors 0  dropped 0  overruns 0  frame 0
+            TX packets 139657  bytes 56363667 (53.7 MiB)
+            TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+    lo: flags=73<UP,LOOPBACK,RUNNING>  mtu 65536
+            inet 127.0.0.1  netmask 255.0.0.0
+            inet6 ::1  prefixlen 128  scopeid 0x10<host>
+            loop  txqueuelen 0  (Local Loopback)
+            RX packets 0  bytes 0 (0.0 B)
+            RX errors 0  dropped 0  overruns 0  frame 0
+            TX packets 0  bytes 0 (0.0 B)
+            TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+    [admin@localhost ~]$
+#>
+function Invoke-SSHStreamExpectSecureAction
+{
+    [CmdletBinding(DefaultParameterSetName='String')]
+    [OutputType([int])]
+    Param
+    (
+        # SSH Shell Stream. 
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   ValueFromPipeline=$true,
+                   Position=0)]
+        [Renci.SshNet.ShellStream]
+        $ShellStream,
+
+        # Initial command that will generate the output to be evaluated by the expect pattern.
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+        [string]
+        $Command,
+
+        # String on what to trigger the action on.
+        [Parameter(Mandatory=$true,
+                   ParameterSetName='String',
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=2)]
+        [string]
+        $ExpectString,
+
+        # Regular expression on what to trigger the action on.
+        [Parameter(Mandatory=$true,
+                   ParameterSetName='Regex',
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=2)]
+        [regex]
+        $ExpectRegex,
+
+        # SecureString representation of action once an expression is matched.
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=3)]
+        [securestring]
+        $SecureAction,
+
+        # Number of seconds to wait for a match.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=4)]
+        [int]
+        $TimeOut = 10
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        Write-Verbose -Message "Executing command $($Command)."
+        $ShellStream.WriteLine($Command)
+        Write-Verbose -Message "Waiting for match."
+        switch ($PSCmdlet.ParameterSetName)
+        {
+            'string' {
+                Write-Verbose -Message 'Matching by String.'
+                $found = $ShellStream.Expect($ExpectString, (New-TimeSpan -Seconds $TimeOut))
+             }
+
+            'Regex'  {
+                Write-Verbose -Message 'Matching by RegEx.'
+                $found = $ShellStream.Expect($ExpectRegex, (New-TimeSpan -Seconds $TimeOut))
+             }
+        }
+        
+        if ($found -ne $null)
+        {
+            Write-Verbose -Message "Executing action."
+            $ShellStream.WriteLine([Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureAction)))
+            Write-Verbose -Message 'Action has been executed.'
+            $SecureAction.Dispose()
+            
+        }
+        else
+        {
+            Write-Warning -Message 'Expect timeout without achiving a match.'
+        }
+    }
+    End
+    {
+    }
+}

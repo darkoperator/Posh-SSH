309664b0e29c4e4941a9f6bbcdd68cf003ddab94
Stephen Mills <stephen.mills@live.com>
2015-06-08 16:35:55 -0600

    Added ComputerName to Get-SSHSession
    Added the ability to find all sshsession with a specific
    ComputerName/Hostname to Get-SSHSession.  Made "Index" the default
    ParameterSetName so no existing scripts should break.

--- Posh-SSH.psm1
+++ Posh-SSH.psm1
@@ -25,38 +25,72 @@ if (!(Test-Path variable:Global:SFTPSessions ))
 # .ExternalHelp Posh-SSH.psm1-Help.xml
 function Get-SSHSession 
 {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='Index')]
     param( 
         [Parameter(Mandatory=$false,
+                   ParameterSetName = 'Index',
                    Position=0)]
         [Alias('Index')]
         [Int32[]]
-        $SessionId
+        $SessionId,
+
+        [Parameter(Mandatory=$false,
+                   ParameterSetName = 'ComputerName',
+                   Position=0)]
+        [Alias('Server', 'HostName', 'Host')]        
+        [string[]]
+        $ComputerName,
+
+        [Parameter(Mandatory=$false,
+                   ParameterSetName = 'ComputerName',
+                   Position=0)]        
+        [switch]
+        $ExactMatch
     )
 
     Begin{}
     Process
     {
-        if ( $PSBoundParameters.ContainsKey('SessionId') )
+        if ($PSCmdlet.ParameterSetName -eq 'Index')
         {
-            foreach($i in $SessionId)
+            if ( $PSBoundParameters.ContainsKey('SessionId') )
             {
-                foreach($session in $SshSessions)
+                foreach($i in $SessionId)
                 {
-                    if ($session.SessionId -eq $i)
+                    foreach($session in $SshSessions)
                     {
-                        $session
+                        if ($session.SessionId -eq $i)
+                        {
+                            $session
+                        }
                     }
                 }
             }
+            else
+            {
+                # Can not reference SShSessions directly so as to be able
+                # to remove the sessions when Remove-SSHSession is used
+                $return_sessions = @()
+                foreach($s in $SshSessions){$return_sessions += $s}
+                $return_sessions
+            }
         }
-        else
+        else # ParameterSetName -eq 'ComputerName'
         {
-            # Can not reference SShSessions directly so as to be able
-            # to remove the sessions when Remove-SSHSession is used
-            $return_sessions = @()
-            foreach($s in $SshSessions){$return_sessions += $s}
-            $return_sessions
+            # Only check to see if it contains ComputerName.  If it get's it without having any values somehow, then don't return anything as they did something odd.
+            if ( $PSBoundParameters.ContainsKey('ComputerName') )
+            {
+                foreach($s in $ComputerName)
+                {
+                    foreach($session in $SshSessions)
+                    {
+                        if ($session.Host -like $s -and ( -not $ExactMatch -or $session.Host -eq $s ) )
+                        {
+                            $session
+                        }
+                    }
+                }
+            }
         }
     }
     End{}

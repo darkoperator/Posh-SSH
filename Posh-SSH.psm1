9a470f9682a1d4183d42e64af241b1e301bf9225
Carlos Perez <carlos_perez@darkoperator.com>
2019-01-12 20:25:09 -0400

    New function Invoke-SSHStreamShellCommand Runs commands on Shellstreams and waits for a prompt before showing output.

--- Posh-SSH.psm1
+++ Posh-SSH.psm1
@@ -848,3 +848,62 @@ function Invoke-SSHStreamExpectSecureAction
     }
 }
 
+# .ExternalHelp Posh-SSH.psm1-Help.xml
+function Invoke-SSHStreamShellCommand {
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param (
+        # SSH stream to use for command execution.
+        [Parameter(Mandatory = $true,
+                   ValueFromPipelineByPropertyName = $true,
+                   Position = 0)]
+        [Renci.SshNet.ShellStream]
+        $ShellStream,
+
+        # Command to execute on SSHStream.
+        [Parameter(Mandatory = $true,
+                   ValueFromPipelineByPropertyName = $true,
+                   Position = 1)]
+        [string]
+        $Command,
+
+        [Parameter(Mandatory = $false,
+                   ValueFromPipelineByPropertyName = $true)]
+        [string]
+        $PrompPattern = '[\$%#>] $'
+    )
+
+    Begin {
+        $promptRegEx = [regex]$PrompPattern
+    }
+    Process {
+        # Discard any banner or previous command output
+        do { 
+            $ShellStream.read() | Out-Null
+    
+        } while ($ShellStream.DataAvailable)
+
+        $ShellStream.writeline($Command)
+
+        #discard line with command entered
+        $ShellStream.ReadLine() | Out-Null
+        Start-Sleep -Milliseconds 500
+
+        $out = ''
+
+        # read all output until there is no more
+        do { 
+            $out += $ShellStream.read()
+    
+        } while ($ShellStream.DataAvailable)
+
+        $outputLines = $out.Split("`n")
+        foreach ($line in $outputLines) {
+            if ($line -notmatch $promptRegEx) {
+                $line
+            }
+        }
+    }
+    End{}
+}

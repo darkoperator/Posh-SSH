aa3ca583b1b5230aff61ac86c3a3b1a3d5d24402
Max Kozlov <{ID}+{username}@users.noreply.github.com>
2021-01-12 15:57:00 +0300

    Extract session creation into method

--- Source/PoshSSH/PoshSSH/NewSessionBase.cs
+++ Source/PoshSSH/PoshSSH/NewSessionBase.cs
@@ -186,11 +186,7 @@ namespace SSH
                 }
             }
         }
-
-        protected override void ProcessRecord()
-        {
-            foreach (var computer in ComputerName)
-            {
+        protected BaseClient CreateConnection(string computer) {
                 ConnectionInfo connectInfo = null;
                 switch (ParameterSetName)
                 {
@@ -325,10 +321,7 @@ namespace SSH
                     // Connect to host using Connection info
                     client.Connect();
 
-                    if (Protocol == PoshSessionType.SSH )
-                        WriteObject(SshModHelper.AddToSshSessionCollection(client as SshClient, SessionState), true);
-                    else
-                        WriteObject(SshModHelper.AddToSftpSessionCollection(client as SftpClient, SessionState), true);
+                    return client;
                 }
                 catch (SshConnectionException e)
                 {
@@ -350,10 +343,24 @@ namespace SSH
                     ErrorRecord erec = new ErrorRecord(e, null, ErrorCategory.InvalidOperation, client);
                     WriteError(erec);
                 }
+                return default;
 
                 // Renci.SshNet.Common.SshOperationTimeoutException when host is not alive or connection times out.
                 // Renci.SshNet.Common.SshConnectionException when fingerprint mismatched
                 // Renci.SshNet.Common.SshAuthenticationException Bad password
+        }
+
+        protected override void ProcessRecord()
+        {
+            foreach (var computer in ComputerName)
+            {
+                var client = CreateConnection(computer);
+                if (client != default) {
+                    if (Protocol == PoshSessionType.SSH)
+                        WriteObject(SshModHelper.AddToSshSessionCollection(client as SshClient, SessionState), true);
+                    else
+                        WriteObject(SshModHelper.AddToSftpSessionCollection(client as SftpClient, SessionState), true);
+                }
             }
 
         } // End process record

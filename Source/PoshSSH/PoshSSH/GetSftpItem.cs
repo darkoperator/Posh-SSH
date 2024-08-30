c62b57c8ea889b968f133fb6e158a44788fd3baf
Carlos Perez <carlos_perez@darkoperator.com>
2024-08-30 19:13:32 -0400

    Update GetSftpItem.cs
    * Make it more resilient when linked files, namedpipes, block devices are in a folder being downloaded.
    * Fix issue when downloading a directory it would create the full path if given at the destination.

--- Source/PoshSSH/PoshSSH/GetSftpItem.cs
+++ Source/PoshSSH/PoshSSH/GetSftpItem.cs
@@ -79,7 +79,7 @@ namespace SSH
         /// If the local file exists overwrite it.
         /// </summary>
         [Parameter(Position = 3,
-            HelpMessage = "Overrite item on remote host if it already pressent.")]
+            HelpMessage = "Overwrite item on remote host if it already present.")]
         public SwitchParameter Force
         {
             get { return _overwrite; }
@@ -105,13 +105,11 @@ namespace SSH
         {
             // Collect the sessions we will upload to.
             var toProcess = new List<SftpSession>();
-            //var toProcess = new SSH.SftpSession[];
             base.BeginProcessing();
             var sessionvar = SessionState.PSVariable.GetValue("Global:SftpSessions") as List<SftpSession>;
             switch (ParameterSetName)
             {
                 case "Session":
-                    // fix issue #37: Get-SFTPFile/Set-SFTPFile fail with 'Object reference not set to an instance of an object.'
                     toProcess.AddRange(_session);
                     ToProcess = toProcess;
                     break;
@@ -130,7 +128,7 @@ namespace SSH
                     break;
                 default:
                     throw new ArgumentException("Bad ParameterSet Name");
-            } // switch (ParameterSetName...
+            }
         }
 
         protected override void ProcessRecord()
@@ -143,7 +141,6 @@ namespace SSH
 
             if (Directory.Exists(@localfullPath))
             {
-
                 foreach (var sftpSession in ToProcess)
                 {
                     foreach (string remotepath in _remotepath)
@@ -156,14 +153,14 @@ namespace SSH
 
                             if (attribs.IsDirectory)
                             {
-                                string dirName = System.IO.Path.GetDirectoryName(remotepath);
-                                var fileFullPath = $"{@localfullPath}{System.IO.Path.DirectorySeparatorChar}{dirName}";
+                                string dirName = System.IO.Path.GetFileName(remotepath.TrimEnd('/'));
+                                var fileFullPath = System.IO.Path.Combine(@localfullPath, dirName);
 
                                 var present = Directory.Exists(fileFullPath);
                                 if (!present || _overwrite)
                                 {
                                     Directory.CreateDirectory(fileFullPath);
-                                    DownloadDirectory(sftpSession.Session, remotepath, fileFullPath, _skipsymlink);
+                                    DownloadDirectory(sftpSession.Session, remotepath, fileFullPath, _skipsymlink, remotepath);
                                 }
                                 else
                                 {
@@ -178,36 +175,13 @@ namespace SSH
                             else if (attribs.IsRegularFile)
                             {
                                 var fileName = System.IO.Path.GetFileName(remotepath);
-
-                                var fileFullPath = $"{@localfullPath}{System.IO.Path.DirectorySeparatorChar}{fileName}";
+                                var fileFullPath = System.IO.Path.Combine(@localfullPath, fileName);
 
                                 var present = File.Exists(fileFullPath);
 
                                 if (!present || _overwrite)
                                 {
-                                    using (var localstream = File.Create(fileFullPath))
-                                    {
-                                        try
-                                        {
-                                            WriteVerbose($"Downloading: {remotepath}");
-                                            var progressHelper = new OperationProgressHelper(this, "Download", remotepath, attribs.Size, 1);
-                                            sftpSession.Session.DownloadFile(remotepath, localstream, progressHelper.Callback);
-                                            progressHelper.Complete();
-                                        }
-                                        catch
-                                        {
-                                            var ex = new SftpPermissionDeniedException($"Unable to download {remotepath} from host.");
-                                            WriteError(new ErrorRecord(
-                                                ex,
-                                               $"Unable to download {remotepath} from host.",
-                                                ErrorCategory.InvalidOperation,
-                                                sftpSession));
-                                        }
-                                        finally
-                                        {
-                                            localstream.Close();
-                                        }
-                                    }
+                                    DownloadFile(sftpSession.Session, sftpSession.Session.Get(remotepath), @localfullPath);
                                 }
                                 else
                                 {
@@ -218,13 +192,7 @@ namespace SSH
                                                      ErrorCategory.InvalidOperation,
                                                      sftpSession));
                                 }
-
                             }
-                            else
-                            {
-                                 
-                            }
-                            
                         }
                         else
                         {
@@ -241,66 +209,126 @@ namespace SSH
             }
             else
             {
-                var ex = new FileNotFoundException("Local path" + localfullPath + " was not found.");
+                var ex = new FileNotFoundException("Local path " + localfullPath + " was not found.");
 
                 ThrowTerminatingError(new ErrorRecord(
                                                 ex,
-                                                "Local path" + localfullPath + " was not found.",
+                                                "Local path " + localfullPath + " was not found.",
                                                 ErrorCategory.InvalidOperation,
                                                 localfullPath));
             }
         }
 
-        private void DownloadDirectory(SftpClient client, string source, string localPath, bool skipsymlink)
+        private void DownloadDirectory(SftpClient client, string source, string localPath, bool skipsymlink, string basePath)
         {
-            var files = client.ListDirectory(source);
-            foreach (var file in files)
+            try
             {
-                if (!file.IsDirectory && !file.IsSymbolicLink)
+                var files = client.ListDirectory(source);
+                foreach (var file in files)
                 {
-                    DownloadFile(client, (SftpFile)file, localPath);
-                }
-                else if (file.IsSymbolicLink)
-                {
-                    var attribs = client.GetAttributes(source);
-                    if (skipsymlink)
-                    {
-                        WriteVerbose($"Skipping symbolic link {file.FullName}");
-                    }
-                    else
-                    {
-                        if (attribs.IsDirectory)
-                        {
-                            var localFullPath = System.IO.Path.Combine(localPath, file.Name);
-                            var dir = Directory.CreateDirectory(localFullPath);
-                            DownloadDirectory(client, file.FullName, dir.FullName, skipsymlink);
-                        }
-                        else if (attribs.IsRegularFile)
-                        {
-                            DownloadFile(client, (SftpFile)file, localPath);
-                        }
-                    }
+                    WriteVerbose($"Processing {file.FullName}");
+                    string relativePath = file.FullName.Substring(basePath.Length).TrimStart('/');
+                    string localFilePath = System.IO.Path.Combine(localPath, relativePath);
 
+                    try
+                    {
+                        if (ShouldDownloadFile(file))
+                        {
+                            DownloadFile(client, file, System.IO.Path.GetDirectoryName(localFilePath));
+                        }
+                        else if (file.IsSymbolicLink)
+                        {
+                            var attribs = client.GetAttributes(file.FullName);
+                            if (skipsymlink)
+                            {
+                                WriteVerbose($"Skipping symbolic link {file.FullName}");
+                            }
+                            else
+                            {
+                                if (attribs.IsDirectory)
+                                {
+                                    Directory.CreateDirectory(localFilePath);
+                                    DownloadDirectory(client, file.FullName, localPath, skipsymlink, basePath);
+                                }
+                                else if (ShouldDownloadFile(file))
+                                {
+                                    DownloadFile(client, file, System.IO.Path.GetDirectoryName(localFilePath));
+                                }
+                            }
+                        }
+                        else if (file.IsDirectory && file.Name != "." && file.Name != "..")
+                        {
+                            Directory.CreateDirectory(localFilePath);
+                            DownloadDirectory(client, file.FullName, localPath, skipsymlink, basePath);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        WriteWarning($"Error processing {file.FullName}: {ex.Message}");
+                        // Continue with the next file
+                    }
                 }
-                else if (file.Name != "." && file.Name != "..")
-                {
-                    var localFullPath = System.IO.Path.Combine(localPath, file.Name);
-                    var dir = Directory.CreateDirectory(localFullPath);
-                    DownloadDirectory(client, file.FullName, dir.FullName, skipsymlink);
-                }
+            }
+            catch (Exception ex)
+            {
+                WriteWarning($"Error accessing directory {source}: {ex.Message}");
+                // The directory download will stop here, but the overall process will continue
             }
         }
 
-        private void DownloadFile(SftpClient client, SftpFile file, string localDirectory)
+        private bool ShouldDownloadFile(ISftpFile file)
         {
-            WriteVerbose($"Downloading {file.FullName}");
-            var localFullPath = System.IO.Path.Combine(localDirectory, file.Name);
-            // Setup Action object for showing download progress.
-            var progressHelper = new OperationProgressHelper(this, "Download", file.Name, file.Length, 1);
-            using (Stream fileStream = File.Create(localFullPath))
+            if (file.IsRegularFile &&
+                !file.IsSocket &&
+                //!file.IsSymbolicLink &&
+                !file.IsBlockDevice &&
+                //!file.IsCharacterDevice &&
+                !file.IsNamedPipe)
             {
-                client.DownloadFile(file.FullName, fileStream, progressHelper.Callback);
-                progressHelper.Complete();
+                return true;
+            }
+            else
+            {
+                string fileType = GetFileType(file);
+                WriteVerbose($"Skipping file {file.FullName}. File type: {fileType}");
+                return false;
+            }
+        }
+
+        private string GetFileType(ISftpFile file)
+        {
+            if (file.IsSocket) return "Socket";
+            if (file.IsSymbolicLink) return "Symbolic Link";
+            if (file.IsBlockDevice) return "Block Device";
+            if (file.IsCharacterDevice) return "Character Device";
+            if (file.IsNamedPipe) return "Named Pipe";
+            if (file.IsDirectory) return "Directory";
+            if (file.IsRegularFile) return "Regular File";
+            return "Unknown";
+        }
+
+        private void DownloadFile(SftpClient client, ISftpFile file, string localDirectory)
+        {
+            if (ShouldDownloadFile(file))
+            {
+                WriteVerbose($"Downloading {file.FullName}");
+                try
+                {
+                    Directory.CreateDirectory(localDirectory); // Ensure the directory exists
+                    var localFullPath = System.IO.Path.Combine(localDirectory, file.Name);
+                    // Setup Action object for showing download progress.
+                    var progressHelper = new OperationProgressHelper(this, "Download", file.Name, file.Length, 1);
+                    using (Stream fileStream = File.Create(localFullPath))
+                    {
+                        client.DownloadFile(file.FullName, fileStream, progressHelper.Callback);
+                        progressHelper.Complete();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    WriteWarning($"Error downloading {file.FullName}: {ex.Message}");
+                    // The individual file download failed, but we'll continue with the next file
+                }
             }
         }
     }
